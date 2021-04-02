8b8d541a7c0f7e7d8b40dcaa582b15546960c0bc
Myma <63861212+MymaQc@users.noreply.github.com>
2021-04-02 17:37:53 -0400

    Fix knockback system ?

--- ZenoPractice/src/Zeno/CustomPlayer.php
+++ ZenoPractice/src/Zeno/CustomPlayer.php
@@ -16,61 +16,37 @@ class CustomPlayer extends Player {
      * @param float $z
      * @param float $base
      */
-    public function knockBack(Entity $attacker, float $damage, float $x, float $z, float $base = 0.4) : void{
-        if (Core::$data->exists($this->getLevel()->getFolderName())) {
-            $data = Core::$data->get($this->getLevel()->getFolderName());
-            $basex = $data["x"];
-            $basey = $data["y"];
-            $basez = $data["z"];
-        } else {
-            $data = Core::$data;
-            $basex = $data->get("default-x");
-            $basey = $data->get("default-y");
-            $basez = $data->get("default-z");
-        }
 
-        $f = sqrt($x * $x + $z * $z);
-        if ($f <= 0){
-            return;
-        }
+    public function knockBack(Entity $attacker, float $damage, float $x, float $z, float $base = 0.4): void {
+        if ($attacker instanceof self) {
+            $f = sqrt($x * $x + $z * $z);
 
-        if (mt_rand() / mt_getrandmax() > $this->getAttributeMap()->getAttribute(Attribute::KNOCKBACK_RESISTANCE)->getValue()) {
-            $f = 1 / $f;
-
-            $motion = clone $this->motion;
-
-            $motion->x /= 2;
-            $motion->y /= 2;
-            $motion->z /= 2;
-            $motion->x += $x * $f * $basex;
-            $motion->y += $basey;
-            $motion->z += $z * $f * $basez;
-
-            if($motion->y > $basey){
-                $motion->y = $basey;
+            if ($f <= 0){
+                return;
             }
 
-            $this->setMotion($motion);
-        }
-    }
+            if (mt_rand() / mt_getrandmax() > $this->getAttributeMap()->getAttribute(Attribute::KNOCKBACK_RESISTANCE)->getValue()){
+                $f = 1 / $f;
 
-    public function attack(EntityDamageEvent $source) : void {
-        parent::attack($source);
-        if (Core::$data->exists($this->getLevel()->getFolderName())) {
-            $data = Core::$data->get($this->getLevel()->getFolderName())["cooldown"];
-        } else {
-            $data = Core::$data->get("default-cooldown");
-        }
+                $motion = clone $this->motion;
 
-        if ($source->isCancelled()) {
+                $motion->x /= 2;
+                $motion->y /= 2;
+                $motion->z /= 2;
+                $motion->x += $x * $f * $base * 1.0685;
+                $motion->y += $base * 0.97;
+                $motion->z += $z * $f * $base * 1.0685;
+                var_dump($motion);
+
+                if ($motion->y > $base) {
+                    $motion->y = $base;
+                }
+
+                $this->setMotion($motion);
+            }
             return;
         }
-
-        $attackSpeed = $data;
-
-        if ($attackSpeed < 0) {
-            $attackSpeed = 0;
-        } $this->attackTime = $attackSpeed;
+        parent::knockBack($attacker, $damage, $x, $z, $base);
     }
 
 }
